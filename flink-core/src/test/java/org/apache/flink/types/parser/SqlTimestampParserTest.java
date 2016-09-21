d23ef2bbbca1c3804ad97d55682164b444035579
twalthr <twalthr@apache.org>
2016-09-21 10:36:40 +0200

    Generic formatted string parsing

--- flink-core/src/test/java/org/apache/flink/types/parser/SqlTimestampParserTest.java
+++ flink-core/src/test/java/org/apache/flink/types/parser/SqlTimestampParserTest.java
@@ -27,7 +27,7 @@ public class SqlTimestampParserTest extends ParserTestBase<Timestamp> {
 	@Override
 	public String[] getValidTestValues() {
 		return new String[] {
-			"1970-01-01 00:00:00.000", "1990-10-14 02:42:25.123", "1990-10-14 02:42:25.123000001",
+			"1970-01-01 00:00:00.000", "1990-10-14 02:42:25", "1990-10-14 02:42:25.123", "1990-10-14 02:42:25.123000001",
 			"1990-10-14 02:42:25.123000002", "2013-08-12 14:15:59.478", "2013-08-12 14:15:59.47",
 			"0000-01-01 00:00:00.000",
 		};
@@ -36,7 +36,7 @@ public class SqlTimestampParserTest extends ParserTestBase<Timestamp> {
 	@Override
 	public Timestamp[] getValidTestResults() {
 		return new Timestamp[] {
-			Timestamp.valueOf("1970-01-01 00:00:00.000"), Timestamp.valueOf("1990-10-14 02:42:25.123"),
+			Timestamp.valueOf("1970-01-01 00:00:00.000"), Timestamp.valueOf("1990-10-14 02:42:25"), Timestamp.valueOf("1990-10-14 02:42:25.123"),
 			Timestamp.valueOf("1990-10-14 02:42:25.123000001"), Timestamp.valueOf("1990-10-14 02:42:25.123000002"),
 			Timestamp.valueOf("2013-08-12 14:15:59.478"), Timestamp.valueOf("2013-08-12 14:15:59.47"),
 			Timestamp.valueOf("0000-01-01 00:00:00.000")
@@ -48,7 +48,7 @@ public class SqlTimestampParserTest extends ParserTestBase<Timestamp> {
 		return new String[] {
 			" 2013-08-12 14:15:59.479", "2013-08-12 14:15:59.479 ", "1970-01-01 00:00::00",
 			"00x00:00", "2013/08/12", "0000-01-01 00:00:00.f00", "2013-08-12 14:15:59.4788888888888888",
-			" ", "\t"
+			" ", "\t", "2010-12-12 15:67:01.123"
 		};
 	}
 
