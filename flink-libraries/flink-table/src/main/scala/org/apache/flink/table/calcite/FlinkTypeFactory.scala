6bae3622a7e27e35d13a9054676012145d0927c5
Haohui Mai <wheat9@apache.org>
2017-02-27 14:24:08 -0800

    [FLINK-5414] [table] Bump up Calcite version to 1.11. (Jark Wu and Haohui Mai)
    This closes #3338.
    This closes #3426.

--- flink-libraries/flink-table/src/main/scala/org/apache/flink/table/calcite/FlinkTypeFactory.scala
+++ flink-libraries/flink-table/src/main/scala/org/apache/flink/table/calcite/FlinkTypeFactory.scala
@@ -131,14 +131,21 @@ class FlinkTypeFactory(typeSystem: RelDataTypeSystem) extends JavaTypeFactoryImp
   }
 
   override def createTypeWithNullability(
-    relDataType: RelDataType,
-    nullable: Boolean)
-  : RelDataType = relDataType match {
-    case composite: CompositeRelDataType =>
-      // at the moment we do not care about nullability
-      composite
-    case _ =>
-      super.createTypeWithNullability(relDataType, nullable)
+      relDataType: RelDataType,
+      nullable: Boolean)
+    : RelDataType = {
+
+    val t = relDataType match {
+      case composite: CompositeRelDataType =>
+        // at the moment we do not care about nullability
+        composite
+      case array: ArrayRelDataType =>
+        val elementType = canonize(createTypeWithNullability(array.getComponentType, nullable))
+        new ArrayRelDataType(array.typeInfo, elementType, nullable)
+      case _ =>
+        super.createTypeWithNullability(relDataType, nullable)
+    }
+    canonize(t)
   }
 }
 
