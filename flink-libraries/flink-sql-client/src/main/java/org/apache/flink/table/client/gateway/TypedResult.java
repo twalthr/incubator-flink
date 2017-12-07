401b5b542c33e08bef663613f2067f7839f55818
twalthr <twalthr@apache.org>
2017-12-07 13:46:31 +0100

    [FLINK-8607] [table] Add a basic embedded SQL CLI client

---
+++ flink-libraries/flink-sql-client/src/main/java/org/apache/flink/table/client/gateway/TypedResult.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.client.gateway;
+
+/**
+ * Result with an attached type (actual payload, EOS, etc.).
+ *
+ * @param <P> type of payload
+ */
+public class TypedResult<P> {
+
+	private final Type type;
+
+	private final P payload;
+
+	private TypedResult(Type type, P payload) {
+		this.type = type;
+		this.payload = payload;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public P getPayload() {
+		return payload;
+	}
+
+	@Override
+	public String toString() {
+		return "TypedResult<" + type + ">";
+	}
+
+	// --------------------------------------------------------------------------------------------
+
+	public static <T> TypedResult<T> empty() {
+		return new TypedResult<>(Type.EMPTY, null);
+	}
+
+	public static <T> TypedResult<T> payload(T payload) {
+		return new TypedResult<>(Type.PAYLOAD, payload);
+	}
+
+	public static <T> TypedResult<T> endOfStream() {
+		return new TypedResult<>(Type.EOS, null);
+	}
+
+	// --------------------------------------------------------------------------------------------
+
+	/**
+	 * Result types.
+	 */
+	public enum Type {
+		PAYLOAD,
+		EMPTY,
+		EOS
+	}
+}
