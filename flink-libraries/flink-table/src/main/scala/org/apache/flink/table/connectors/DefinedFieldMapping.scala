104caaa532c4d480432b646cf88b1bb21a5b2297
Timo Walther <twalthr@apache.org>
2018-07-17 11:57:17 +0200

    Update docs for moved classes

--- flink-libraries/flink-table/src/main/scala/org/apache/flink/table/connectors/DefinedFieldMapping.scala
+++ flink-libraries/flink-table/src/main/scala/org/apache/flink/table/connectors/DefinedFieldMapping.scala
@@ -22,37 +22,43 @@ import java.util.{Map => JMap}
 import javax.annotation.Nullable
 
 import org.apache.flink.api.common.typeinfo.TypeInformation
-import org.apache.flink.table.api.TableSchema
+import org.apache.flink.table.sinks.TableSink
 import org.apache.flink.table.sources.TableSource
 import org.apache.flink.table.sources.tsextractors.TimestampExtractor
 
 /**
-  * The [[DefinedFieldMapping]] interface provides a mapping for the fields of the table schema
-  * ([[TableSource.getTableSchema]] to fields of the physical returned type
-  * [[TableSource.getReturnType]] of a [[TableSource]].
+  * The [[DefinedFieldMapping]] interface provides a mapping for the fields of the table schema to
+  * fields of the physical returned type.
   *
-  * If a [[TableSource]] does not implement the [[DefinedFieldMapping]] interface, the fields of
-  * its [[TableSchema]] are mapped to the fields of its return type [[TypeInformation]] by name.
+  * For [[TableSource]] implementations this interface maps [[TableSource.getTableSchema]] to fields
+  * of the physical returned type [[TableSource.getReturnType]].
+  *
+  * For [[TableSink]] implementations this interface maps [[TableSink.getFieldNames]] and
+  * [[TableSink.getFieldTypes]] to fields of the physical output type [[TableSink.getOutputType]].
+  *
+  * If a table source or sink does not implement this interface, the fields are mapped from schema
+  * to the fields of the physical output type in [[TypeInformation]] by name.
   *
   * If the fields cannot or should not be implicitly mapped by name, an explicit mapping can be
   * provided by implementing this interface.
+  *
   * If a mapping is provided, all fields must be explicitly mapped.
   */
 trait DefinedFieldMapping {
 
   /**
-    * Returns the mapping for the fields of the [[TableSource]]'s [[TableSchema]] to the fields of
-    * its return type [[TypeInformation]].
+    * Returns the mapping for the fields of the table schema to fields of the physical returned
+    * type.
     *
     * The mapping is done based on field names, e.g., a mapping "name" -> "f1" maps the schema field
-    * "name" to the field "f1" of the return type, for example in this case the second field of a
+    * "name" to the field "f1" of the output type, for example in this case the second field of a
     * [[org.apache.flink.api.java.tuple.Tuple]].
     *
-    * The returned mapping must map all fields (except proctime and rowtime fields) to the return
-    * type. It can also provide a mapping for fields which are not in the [[TableSchema]] to make
+    * The returned mapping must map all fields (except proctime and rowtime fields) to the output
+    * type. It can also provide a mapping for fields which are not in the table schema to make
     * fields in the physical [[TypeInformation]] accessible for a [[TimestampExtractor]].
     *
-    * @return A mapping from [[TableSchema]] fields to [[TypeInformation]] fields or
+    * @return A mapping from table schema fields to [[TypeInformation]] fields or
     *         null if no mapping is necessary.
     */
   @Nullable
--- flink-libraries/flink-table/src/main/scala/org/apache/flink/table/connectors/DefinedProctimeAttribute.scala
+++ flink-libraries/flink-table/src/main/scala/org/apache/flink/table/connectors/DefinedProctimeAttribute.scala
@@ -2,11 +2,12 @@ package org.apache.flink.table.connectors
 
 import javax.annotation.Nullable
 
-import org.apache.flink.table.api.{TableSchema, Types}
+import org.apache.flink.table.api.Types
+import org.apache.flink.table.sinks.TableSink
 import org.apache.flink.table.sources.TableSource
 
 /**
-  * Extends a [[TableSource]] to specify a processing time attribute.
+  * Extends a [[TableSource]] or [[TableSink]] to specify a processing time attribute.
   */
 trait DefinedProctimeAttribute {
 
@@ -14,8 +15,8 @@ trait DefinedProctimeAttribute {
     * Returns the name of a processing time attribute or null if no processing time attribute is
     * present.
     *
-    * The referenced attribute must be present in the [[TableSchema]] of the [[TableSource]] and of
-    * type [[Types.SQL_TIMESTAMP]].
+    * The referenced attribute must be present in the table schema of a [[TableSource]] or
+    * [[TableSink]] with type [[Types.SQL_TIMESTAMP]].
     */
   @Nullable
   def getProctimeAttribute: String
