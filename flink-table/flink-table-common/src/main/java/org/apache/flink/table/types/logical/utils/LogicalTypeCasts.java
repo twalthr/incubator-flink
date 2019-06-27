23e62061165031299f11412e8151ec9f4f58cb08
Timo Walther <twalthr@apache.org>
2019-06-27 13:15:29 +0200

    fixes

--- flink-table/flink-table-common/src/main/java/org/apache/flink/table/types/logical/utils/LogicalTypeCasts.java
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/types/logical/utils/LogicalTypeCasts.java
@@ -19,15 +19,28 @@
 package org.apache.flink.table.types.logical.utils;
 
 import org.apache.flink.annotation.Internal;
+import org.apache.flink.table.types.logical.ArrayType;
+import org.apache.flink.table.types.logical.BinaryType;
+import org.apache.flink.table.types.logical.CharType;
 import org.apache.flink.table.types.logical.DateType;
+import org.apache.flink.table.types.logical.DecimalType;
 import org.apache.flink.table.types.logical.DistinctType;
+import org.apache.flink.table.types.logical.DoubleType;
 import org.apache.flink.table.types.logical.LogicalType;
 import org.apache.flink.table.types.logical.LogicalTypeFamily;
 import org.apache.flink.table.types.logical.LogicalTypeRoot;
+import org.apache.flink.table.types.logical.MultisetType;
+import org.apache.flink.table.types.logical.NullType;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.table.types.logical.TimeType;
+import org.apache.flink.table.types.logical.VarBinaryType;
+import org.apache.flink.table.types.logical.VarCharType;
 import org.apache.flink.util.Preconditions;
 
 import javax.annotation.Nullable;
 
+import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -36,7 +49,9 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import static org.apache.flink.table.types.logical.LogicalTypeFamily.APPROXIMATE_NUMERIC;
 import static org.apache.flink.table.types.logical.LogicalTypeFamily.BINARY_STRING;
 import static org.apache.flink.table.types.logical.LogicalTypeFamily.CHARACTER_STRING;
 import static org.apache.flink.table.types.logical.LogicalTypeFamily.CONSTRUCTED;
@@ -48,6 +63,7 @@ import static org.apache.flink.table.types.logical.LogicalTypeFamily.PREDEFINED;
 import static org.apache.flink.table.types.logical.LogicalTypeFamily.TIME;
 import static org.apache.flink.table.types.logical.LogicalTypeFamily.TIMESTAMP;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.ANY;
+import static org.apache.flink.table.types.logical.LogicalTypeRoot.ARRAY;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.BIGINT;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.BINARY;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.BOOLEAN;
@@ -60,7 +76,9 @@ import static org.apache.flink.table.types.logical.LogicalTypeRoot.FLOAT;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.INTEGER;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.INTERVAL_DAY_TIME;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.INTERVAL_YEAR_MONTH;
+import static org.apache.flink.table.types.logical.LogicalTypeRoot.MULTISET;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.NULL;
+import static org.apache.flink.table.types.logical.LogicalTypeRoot.ROW;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.SMALLINT;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.STRUCTURED_TYPE;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.SYMBOL;
@@ -71,7 +89,11 @@ import static org.apache.flink.table.types.logical.LogicalTypeRoot.TIME_WITHOUT_
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.TINYINT;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.VARBINARY;
 import static org.apache.flink.table.types.logical.LogicalTypeRoot.VARCHAR;
+import static org.apache.flink.table.types.logical.utils.LogicalTypeChecks.getLength;
+import static org.apache.flink.table.types.logical.utils.LogicalTypeChecks.getPrecision;
+import static org.apache.flink.table.types.logical.utils.LogicalTypeChecks.getScale;
 import static org.apache.flink.table.types.logical.utils.LogicalTypeChecks.hasFamily;
+import static org.apache.flink.table.types.logical.utils.LogicalTypeChecks.hasRoot;
 import static org.apache.flink.table.types.logical.utils.LogicalTypeChecks.isSingleFieldInterval;
 
 /**
@@ -218,14 +240,6 @@ public final class LogicalTypeCasts {
 	 * Flink's type system.
 	 */
 	public static Optional<LogicalType> findCommonType(List<LogicalType> types) {
-		final LogicalType foundType = findLeastRestrictive(types);
-		if (foundType != null) {
-			return Optional.of(foundType);
-		}
-		return Optional.ofNullable(findLeastRestrictiveByCast(types));
-	}
-
-	private static @Nullable LogicalType findLeastRestrictive(List<LogicalType> types) {
 		Preconditions.checkArgument(types.size() > 0);
 
 		// collect statistics first
@@ -248,26 +262,344 @@ public final class LogicalTypeCasts {
 			.map(t -> t.copy(true))
 			.collect(Collectors.toList());
 
-		// ANY types must only be equal
-		if (hasAnyType) {
-			return findSameType(normalizedTypes)
+		LogicalType foundType = findLeastRestrictive(normalizedTypes, hasAnyType, hasNullType);
+		if (foundType == null) {
+			foundType = findLeastRestrictiveByCast(normalizedTypes);
 		}
 
-		return null;
+		if (foundType != null) {
+			final LogicalType typeWithNullability = foundType.copy(hasNullableTypes);
+			return Optional.of(typeWithNullability);
+		}
+		return Optional.empty();
 	}
 
-	private static @Nullable LogicalType findLeastRestrictiveByCast(List<LogicalType> types) {
-		throw new UnsupportedOperationException();
+	private static @Nullable LogicalType findLeastRestrictive(
+			List<LogicalType> normalizedTypes,
+			boolean hasAnyType,
+			boolean hasNullType) {
+
+		// all ANY types must be equal
+		if (hasAnyType) {
+			return findSameType(normalizedTypes);
+		}
+
+		LogicalType resultType = null;
+
+		for (LogicalType type : normalizedTypes) {
+			final LogicalTypeRoot typeRoot = type.getTypeRoot();
+
+			// NULL does not affect the result of this loop
+			if (typeRoot == NULL) {
+				continue;
+			}
+
+			// if result type is still null, consider the current type as a potential
+			// result type candidate
+			if (resultType == null) {
+				resultType = type;
+			}
+
+			// for types of family CONSTRUCTED
+			if (typeRoot == ARRAY) {
+				return findLeastRestrictiveArrayType(normalizedTypes);
+			} else if (typeRoot == MULTISET) {
+				return findLeastRestrictiveMultisetType(normalizedTypes);
+			} else if (typeRoot == ROW) {
+				return findLeastRestrictiveRowType(normalizedTypes);
+			}
+
+			// exit if two completely different types are compared (e.g. ROW and INT)
+			// this simplifies the following lines as we compare same interval families for example
+			if (!areSimilarTypes(resultType, type)) {
+				return null;
+			}
+
+			// for types of family CHARACTER_STRING or BINARY_STRING
+			if (hasFamily(type, CHARACTER_STRING) || hasFamily(type, BINARY_STRING)) {
+				final int length = getMaxLength(resultType, type);
+
+				if (hasRoot(resultType, VARCHAR) || hasRoot(resultType, VARBINARY)) {
+					// for variable length type we are done here
+					resultType = createStringType(resultType.getTypeRoot(), length);
+				} else {
+					// for mixed fixed/variable or fixed/fixed lengths
+					resultType = createStringType(typeRoot, length);
+				}
+			}
+			// for EXACT_NUMERIC types
+			else if (hasFamily(type, EXACT_NUMERIC)) {
+				if (hasFamily(resultType, EXACT_NUMERIC)) {
+					resultType = getLeastRestrictiveExactNumericType(resultType, type);
+				} else if (hasFamily(resultType, APPROXIMATE_NUMERIC)) {
+					// the result is already approximate
+					if (typeRoot == DECIMAL) {
+						// in case of DECIMAL we enforce DOUBLE
+						resultType = new DoubleType();
+					}
+				} else {
+					return null;
+				}
+			}
+			// for APPROXIMATE_NUMERIC types
+			else if (hasFamily(type, APPROXIMATE_NUMERIC)) {
+				if (hasFamily(type, APPROXIMATE_NUMERIC)) {
+					resultType = getLeastRestrictiveApproximateNumericType(resultType, type);
+				} else if (hasFamily(resultType, EXACT_NUMERIC)) {
+					// the result was exact so far
+					if (typeRoot == DECIMAL) {
+						// in case of DECIMAL we enforce DOUBLE
+						resultType = new DoubleType();
+					} else {
+						// enforce an approximate result
+						resultType = type;
+					}
+				} else {
+					return null;
+				}
+			}
+			// for TIME and TIMESTAMP
+			else if (hasFamily(type, TIME)) {
+				if (hasFamily(resultType, TIME)) {
+					resultType = getLeastRestrictiveTimeType(resultType, type);
+				} else {
+					return null;
+				}
+			}
+			// for INTERVAL types
+			else if (hasFamily(type, INTERVAL)) {
+				resultType = getLeastRestrictiveIntervalType(resultType, type);
+			}
+			// other types are handled by leastRestrictiveByCast
+			else {
+				return null;
+			}
+		}
+
+		// NULL type only
+		if (resultType == null && hasNullType) {
+			return new NullType();
+		}
+
+		return resultType;
 	}
 
-	private static @Nullable LogicalType findSameType(List<LogicalType> types) {
-		final LogicalType firstType = types.get(0);
-		for (LogicalType type : types) {
+	private static LogicalType getLeastRestrictiveTimeType(LogicalType resultType, LogicalType type) {
+		// same EXACT_NUMERIC types
+		if (type.equals(resultType)) {
+			return resultType;
+		}
+
+		final int p1 = getPrecision(resultType);
+		final int p2 = getPrecision(type);
+		final int maxPrecision = TimeType.MAX_PRECISION;
+
+		int p = Math.max(p1, p2);
+		p = Math.min(p, maxPrecision);
+
+		return new TimeType(p);
+	}
+
+	private static LogicalType getLeastRestrictiveIntervalType(LogicalType resultType, LogicalType type) {
+		return null; // TODO
+	}
+
+	private static LogicalType getLeastRestrictiveApproximateNumericType(LogicalType resultType, LogicalType type) {
+		if (hasRoot(resultType, DOUBLE) || hasRoot(type, DOUBLE)) {
+			return new DoubleType();
+		}
+		return resultType;
+	}
+
+	private static LogicalType getLeastRestrictiveExactNumericType(LogicalType resultType, LogicalType type) {
+		// same EXACT_NUMERIC types
+		if (type.equals(resultType)) {
+			return resultType;
+		}
+
+		final LogicalTypeRoot resultTypeRoot = resultType.getTypeRoot();
+		final LogicalTypeRoot typeRoot = type.getTypeRoot();
+
+		// no DECIMAL types involved
+		if (resultTypeRoot != DECIMAL && typeRoot != DECIMAL) {
+			// type root contains order of precision
+			if (getPrecision(type) > getPrecision(resultType)) {
+				return type;
+			}
+			return resultType;
+		}
+
+		// determine DECIMAL with precision (p), scale (s) and number of whole digits (d):
+		// d = max(p1 - s1, p2 - s2)
+		// s <= max(s1, s2)
+		// p = s + d
+		final int p1 = getPrecision(resultType);
+		final int p2 = getPrecision(type);
+		final int s1 = getScale(resultType);
+		final int s2 = getScale(type);
+		final int maxPrecision = DecimalType.MAX_PRECISION;
+
+		int d = Math.max(p1 - s1, p2 - s2);
+		d = Math.min(d, maxPrecision);
+
+		int s = Math.max(s1, s2);
+		s = Math.min(s, maxPrecision - d);
+
+		final int p = d + s;
+
+		return new DecimalType(p, s);
+	}
+
+	private static boolean areSimilarTypes(LogicalType left, LogicalType right) {
+		// two types are similar iff they can be the operands of an SQL equality predicate
+
+		// similarity based on families
+		if (hasFamily(left, CHARACTER_STRING) && hasFamily(right, CHARACTER_STRING)) {
+			return true;
+		} else if (hasFamily(left, BINARY_STRING) && hasFamily(right, BINARY_STRING)) {
+			return true;
+		} else if (hasFamily(left, NUMERIC) && hasFamily(right, NUMERIC)) {
+			return true;
+		} else if (hasFamily(left, TIME) && hasFamily(right, TIME)) {
+			return true;
+		} else if (hasFamily(left, TIMESTAMP) && hasFamily(right, TIMESTAMP)) {
+			return true;
+		}
+		// similarity based on root
+		return left.getTypeRoot() == right.getTypeRoot();
+	}
+
+	private static LogicalType createStringType(LogicalTypeRoot typeRoot, int length) {
+		switch (typeRoot) {
+			case CHAR:
+				return new CharType(length);
+			case VARCHAR:
+				return new VarCharType(length);
+			case BINARY:
+				return new BinaryType(length);
+			case VARBINARY:
+				return new VarBinaryType(length);
+			default:
+				throw new IllegalArgumentException();
+		}
+	}
+
+	private static @Nullable LogicalType findLeastRestrictiveMultisetType(List<LogicalType> normalizedTypes) {
+		final List<LogicalType> children = findLeastRestrictiveChildren(normalizedTypes);
+		if (children == null) {
+			return null;
+		}
+		return new MultisetType(children.get(0));
+	}
+
+	private static @Nullable LogicalType findLeastRestrictiveArrayType(List<LogicalType> normalizedTypes) {
+		final List<LogicalType> children = findLeastRestrictiveChildren(normalizedTypes);
+		if (children == null) {
+			return null;
+		}
+		return new ArrayType(children.get(0));
+	}
+
+	private static @Nullable LogicalType findLeastRestrictiveRowType(List<LogicalType> normalizedTypes) {
+		final List<LogicalType> children = findLeastRestrictiveChildren(normalizedTypes);
+		if (children == null) {
+			return null;
+		}
+		final RowType firstType = (RowType) normalizedTypes.get(0);
+		final List<RowType.RowField> newFields = IntStream.range(0, children.size())
+			.mapToObj(pos -> {
+				final LogicalType newType = children.get(pos);
+				final RowType.RowField originalField = firstType.getFields().get(pos);
+				if (originalField.getDescription().isPresent()) {
+					return new RowType.RowField(
+						originalField.getName(),
+						newType,
+						originalField.getDescription().get());
+				} else {
+					return new RowType.RowField(
+						originalField.getName(),
+						newType);
+				}
+			})
+			.collect(Collectors.toList());
+		return new RowType(newFields);
+	}
+
+	private static @Nullable List<LogicalType> findLeastRestrictiveChildren(List<LogicalType> normalizedTypes) {
+		final LogicalType firstType = normalizedTypes.get(0);
+		final LogicalTypeRoot typeRoot = firstType.getTypeRoot();
+		final int numberOfChildren = firstType.getChildren().size();
+
+		for (LogicalType type : normalizedTypes) {
+			// all types must have the same root
+			if (type.getTypeRoot() != typeRoot) {
+				return null;
+			}
+			// all types must have the same number of children
+			if (type.getChildren().size() != numberOfChildren) {
+				return null;
+			}
+		}
+
+		// recursively compute column-wise least restrictive
+		final List<LogicalType> resultChildren = new ArrayList<>(numberOfChildren);
+		for (int i = 0; i < numberOfChildren; i++) {
+			final int childPos = i;
+			final Optional<LogicalType> childType = findCommonType(new AbstractList<LogicalType>() {
+				@Override
+				public LogicalType get(int index) {
+					return normalizedTypes.get(index).getChildren().get(childPos);
+				}
+
+				@Override
+				public int size() {
+					return normalizedTypes.size();
+				}
+			});
+			if (!childType.isPresent()) {
+				return null;
+			}
+			resultChildren.add(childType.get());
+		}
+		// no child should be empty at this point
+		return resultChildren;
+	}
+
+	private static @Nullable LogicalType findSameType(List<LogicalType> normalizedTypes) {
+		final LogicalType firstType = normalizedTypes.get(0);
+		for (LogicalType type : normalizedTypes) {
 			if (!type.equals(firstType)) {
 				return null;
 			}
 		}
 		return firstType;
+	}
+
+	private static @Nullable LogicalType findLeastRestrictiveByCast(List<LogicalType> normalizedTypes) {
+		LogicalType resultType = normalizedTypes.get(0);
+
+		for (LogicalType type : normalizedTypes) {
+			final LogicalTypeRoot typeRoot = type.getTypeRoot();
+
+			// NULL does not affect the result of this loop
+			if (typeRoot == NULL) {
+				continue;
+			}
+
+			if (supportsImplicitCast(resultType, type)) {
+				resultType = type;
+			} else {
+				if (!supportsImplicitCast(type, resultType)) {
+					return null;
+				}
+			}
+		}
+
+		return resultType;
+	}
+
+	private static int getMaxLength(LogicalType left, LogicalType right) {
+		return Math.max(getLength(left), getLength(right));
 	}
 
 	// --------------------------------------------------------------------------------------------
--- flink-table/flink-table-common/src/test/java/org/apache/flink/table/types/LogicalTypeCommonTypeTest.java
+++ flink-table/flink-table-common/src/test/java/org/apache/flink/table/types/LogicalTypeCommonTypeTest.java
@@ -18,7 +18,22 @@
 
 package org.apache.flink.table.types;
 
-import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.types.logical.ArrayType;
+import org.apache.flink.table.types.logical.BigIntType;
+import org.apache.flink.table.types.logical.BinaryType;
+import org.apache.flink.table.types.logical.CharType;
+import org.apache.flink.table.types.logical.DateType;
+import org.apache.flink.table.types.logical.DecimalType;
+import org.apache.flink.table.types.logical.DoubleType;
+import org.apache.flink.table.types.logical.FloatType;
+import org.apache.flink.table.types.logical.IntType;
+import org.apache.flink.table.types.logical.LogicalType;
+import org.apache.flink.table.types.logical.MultisetType;
+import org.apache.flink.table.types.logical.NullType;
+import org.apache.flink.table.types.logical.RowType;
+import org.apache.flink.table.types.logical.TimeType;
+import org.apache.flink.table.types.logical.VarBinaryType;
+import org.apache.flink.table.types.logical.VarCharType;
 import org.apache.flink.table.types.logical.utils.LogicalTypeCasts;
 
 import org.junit.Test;
@@ -29,7 +44,7 @@ import org.junit.runners.Parameterized.Parameters;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
@@ -38,34 +53,173 @@ import static org.junit.Assert.assertThat;
  * Tests for {@link LogicalTypeCasts}.
  */
 @RunWith(Parameterized.class)
-public class LogicalTypeWideningTest {
+public class LogicalTypeCommonTypeTest {
 
 	@Parameters(name = "{index}: [Types: {0}, To: {1}]")
 	public static List<Object[]> testData() {
 		return Arrays.asList(
 			new Object[][]{
-				{DataTypes.SMALLINT(), DataTypes.BIGINT(), true, true},
+
+				// simple types
+				{
+					Arrays.asList(new IntType(), new IntType()),
+					new IntType()
+				},
+
+				// incompatible types
+				{
+					Arrays.asList(new IntType(), new ArrayType(new IntType())),
+					null
+				},
+
+				// incompatible types
+				{
+					Arrays.asList(new IntType(), new VarCharType(23)),
+					null
+				},
+
+				// NOT NULL types
+				{
+					Arrays.asList(new IntType(false), new IntType(false)),
+					new IntType(false)
+				},
+
+				// NOT NULL with different types
+				{
+					Arrays.asList(new IntType(true), new BigIntType(false)),
+					new BigIntType()
+				},
+
+				// NULL only
+				{
+					Arrays.asList(new NullType(), new NullType()),
+					new NullType()
+				},
+
+				// NULL with other types
+				{
+					Arrays.asList(new NullType(), new IntType(), new IntType()),
+					new IntType()
+				},
+
+				// ARRAY types with same element type
+				{
+					Arrays.asList(new ArrayType(new IntType()), new ArrayType(new IntType())),
+					new ArrayType(new IntType())
+				},
+
+				// ARRAY types with different element types
+				{
+					Arrays.asList(new ArrayType(new BigIntType()), new ArrayType(new IntType())),
+					new ArrayType(new BigIntType())
+				},
+
+				// MULTISET types with different element type
+				{
+					Arrays.asList(new MultisetType(new BigIntType()), new MultisetType(new IntType())),
+					new MultisetType(new BigIntType())
+				},
+
+				// ROW type with different element types
+				{
+					Arrays.asList(
+						RowType.of(new IntType(), new IntType(), new BigIntType()),
+						RowType.of(new BigIntType(), new IntType(), new IntType())),
+					RowType.of(new BigIntType(), new IntType(), new BigIntType())
+				},
+
+				// CHAR types of different length
+				{
+					Arrays.asList(new CharType(2), new CharType(4)),
+					new CharType(4)
+				},
+
+				// VARCHAR types of different length
+				{
+					Arrays.asList(new VarCharType(2), new VarCharType(VarCharType.MAX_LENGTH)),
+					new VarCharType(VarCharType.MAX_LENGTH)
+				},
+
+				// mixed VARCHAR and CHAR types
+				{
+					Arrays.asList(new VarCharType(2), new CharType(5)),
+					new VarCharType(5)
+				},
+
+				// more mixed VARCHAR and CHAR types
+				{
+					Arrays.asList(new CharType(5), new VarCharType(2), new VarCharType(7)),
+					new VarCharType(7)
+				},
+
+				// mixed BINARY and VARBINARY types
+				{
+					Arrays.asList(new BinaryType(5), new VarBinaryType(2), new VarBinaryType(7)),
+					new VarBinaryType(7)
+				},
+
+				// two APPROXIMATE_NUMERIC types
+				{
+					Arrays.asList(new DoubleType(), new FloatType()),
+					new DoubleType()
+				},
+
+				// one APPROXIMATE_NUMERIC and one DECIMAL type
+				{
+					Arrays.asList(new DoubleType(), new DecimalType(2, 2)),
+					new DoubleType()
+				},
+
+				// one APPROXIMATE_NUMERIC and one EXACT_NUMERIC type
+				{
+					Arrays.asList(new DoubleType(), new IntType()),
+					new DoubleType()
+				},
+
+				// two APPROXIMATE_NUMERIC and one DECIMAL type
+				{
+					Arrays.asList(new DecimalType(2, 2), new DoubleType(), new FloatType()),
+					new DoubleType()
+				},
+
+				// DECIMAL precision and scale merging
+				{
+					Arrays.asList(new DecimalType(2, 2), new DecimalType(5, 2), new DecimalType(7, 5)),
+					new DecimalType(8, 5)
+				},
+
+				// DECIMAL precision and scale merging with other EXACT_NUMERIC types
+				{
+					Arrays.asList(new DecimalType(2, 2), new IntType(), new BigIntType()),
+					new DecimalType(21, 2)
+				},
+
+				// unsupported time merging
+				{
+					Arrays.asList(new DateType(), new DateType(), new TimeType()),
+					null
+				},
+
+				// time precision merging
+				{
+					Arrays.asList(new TimeType(3), new TimeType(2)),
+					new TimeType(3)
+				}
+
 			}
 		);
 	}
 
 	@Parameter
-	public Set<DataType> types;
+	public List<LogicalType> types;
 
 	@Parameter(1)
-	public DataType result;
+	public LogicalType commonType;
 
 	@Test
-	public void testWidening() {
+	public void testCommonType() {
 		assertThat(
-			LogicalTypeCasts.supportsImplicitCast(sourceType.getLogicalType(), targetType.getLogicalType()),
-			equalTo(supportsImplicit));
-	}
-
-	@Test
-	public void testExplicitCasting() {
-		assertThat(
-			LogicalTypeCasts.supportsExplicitCast(sourceType.getLogicalType(), targetType.getLogicalType()),
-			equalTo(supportsExplicit));
+			LogicalTypeCasts.findCommonType(types),
+			equalTo(Optional.ofNullable(commonType)));
 	}
 }
