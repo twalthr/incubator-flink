3938ad7157fca67863a8492839a08a9ddc215a94
Timo Walther <twalthr@apache.org>
2019-07-22 12:27:01 +0200

    Add test specs next

--- flink-table/flink-table-common/src/test/java/org/apache/flink/table/types/ReflectiveDataTypeConverterTest.java
+++ flink-table/flink-table-common/src/test/java/org/apache/flink/table/types/ReflectiveDataTypeConverterTest.java
@@ -18,15 +18,29 @@
 
 package org.apache.flink.table.types;
 
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.table.functions.TableFunction;
 import org.apache.flink.table.types.utils.ReflectiveDataTypeConverter;
 
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+
+import javax.annotation.Nullable;
+
+import java.util.function.Consumer;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 /**
  * Tests for {@link ReflectiveDataTypeConverter}.
  */
+@RunWith(Parameterized.class)
 public class ReflectiveDataTypeConverterTest {
 
 	@Test
@@ -38,11 +52,89 @@ public class ReflectiveDataTypeConverterTest {
 //		converter.createTypeVariableMapping(type);
 	}
 
-	public static class TestFunction extends TableFunction<Tuple1Level<String>> {
+	@Parameter
+	public TestSpec testSpec;
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
+	@Test
+	public void testClassExtraction() {
+		if (testSpec.clazz != null && testSpec.expectedDataType != null) {
+			assertThat(
+				getConfiguredExtractor().extractDataType(testSpec.clazz),
+				equalTo(testSpec.expectedDataType));
+		}
 	}
 
-	public static class Tuple1Level<T> extends Tuple2<T, Integer> {
-
+	@Test
+	public void testArgumentExtraction() {
+		if (testSpec.function != null && testSpec.expectedDataType != null) {
+			assertThat(
+				getConfiguredExtractor().extractDataType(testSpec.clazz),
+				equalTo(testSpec.expectedDataType));
+		}
 	}
+
+	private ReflectiveDataTypeConverter getConfiguredExtractor() {
+		final ReflectiveDataTypeConverter.Builder builder = ReflectiveDataTypeConverter.newInstance();
+		if (testSpec.configuration != null) {
+			testSpec.configuration.accept(builder);
+		}
+		return builder.build();
+	}
+
+	// --------------------------------------------------------------------------------------------
+
+	private static class TestSpec {
+
+		final @Nullable Class<?> clazz;
+
+		final @Nullable Cl function;
+
+		@Nullable Consumer<ReflectiveDataTypeConverter.Builder> configuration;
+
+		@Nullable DataType expectedDataType;
+
+		@Nullable String expectedErrorMessage;
+
+		TestSpec(Class<?> clazz) {
+			this.clazz = clazz;
+			this.function = null;
+		}
+
+		TestSpec(TableFunction<?> function) {
+			this.clazz = null;
+			this.function = function;
+		}
+
+		static TestSpec forClass(Class<?> clazz) {
+			return new TestSpec(clazz);
+		}
+
+		static TestSpec forArgumentOf(TableFunction<?> function) {
+			return new TestSpec(function);
+		}
+
+		TestSpec configuration(Consumer<ReflectiveDataTypeConverter.Builder> configuration) {
+			this.configuration = configuration;
+			return this;
+		}
+
+		TestSpec expectDataType(DataType expectedDataType) {
+			this.expectedDataType = expectedDataType;
+			return this;
+		}
+
+		TestSpec expectErrorMessage(String expectedErrorMessage) {
+			this.expectedErrorMessage = expectedErrorMessage;
+			return this;
+		}
+	}
+
+	// --------------------------------------------------------------------------------------------
+	// Test classes for extraction
+	// --------------------------------------------------------------------------------------------
+
+	public static class SimpleStructured
 }
