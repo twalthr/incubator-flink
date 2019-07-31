c0d91f09444653b5ea79a4f76829d0120e2afb23
Timo Walther <twalthr@apache.org>
2019-07-31 12:17:51 +0200

    fixes

--- flink-end-to-end-tests/test-scripts/test_sql_client.sh
+++ flink-end-to-end-tests/test-scripts/test_sql_client.sh
@@ -199,7 +199,7 @@ echo "Executing SQL: Values -> Elasticsearch (upsert)"
 SQL_STATEMENT_3=$(cat << EOF
 INSERT INTO ElasticsearchUpsertSinkTable
   SELECT user_id, user_name, COUNT(*) AS user_count
-  FROM (VALUES (1, 'Bob'), (22, 'Alice'), (42, 'Greg'), (42, 'Greg'), (42, 'Greg'), (1, 'Bob'))
+  FROM (VALUES (1, 'Bob'), (22, 'Tom'), (42, 'Kim'), (42, 'Kim'), (42, 'Kim'), (1, 'Bob'))
     AS UserCountTable(user_id, user_name)
   GROUP BY user_id, user_name
 EOF
@@ -213,7 +213,7 @@ JOB_ID=$($FLINK_DIR/bin/sql-client.sh embedded \
 
 wait_job_terminal_state "$JOB_ID" "FINISHED"
 
-verify_result_hash "SQL Client Elasticsearch Upsert" "$ELASTICSEARCH_INDEX" 3 "f16e8da8e0f379b6af01dcdc00cff4e1"
+verify_result_hash "SQL Client Elasticsearch Upsert" "$ELASTICSEARCH_INDEX" 3 "21a76360e2a40f442816d940e7071ccf"
 
 echo "Executing SQL: Values -> Elasticsearch (append, no key)"
 
@@ -223,10 +223,10 @@ INSERT INTO ElasticsearchAppendSinkTable
   FROM (
     VALUES
       (1, 'Bob', CAST(0 AS BIGINT)),
-      (22, 'Alice', CAST(0 AS BIGINT)),
-      (42, 'Greg', CAST(0 AS BIGINT)),
-      (42, 'Greg', CAST(0 AS BIGINT)),
-      (42, 'Greg', CAST(0 AS BIGINT)),
+      (22, 'Tom', CAST(0 AS BIGINT)),
+      (42, 'Kim', CAST(0 AS BIGINT)),
+      (42, 'Kim', CAST(0 AS BIGINT)),
+      (42, 'Kim', CAST(0 AS BIGINT)),
       (1, 'Bob', CAST(0 AS BIGINT)))
     AS UserCountTable(user_id, user_name, user_count)
 EOF
