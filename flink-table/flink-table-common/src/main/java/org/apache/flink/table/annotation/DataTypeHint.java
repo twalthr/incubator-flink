e979eeae7c0ae42db9b9971574b1c8346df59680
Timo Walther <twalthr@apache.org>
2019-11-14 09:27:51 +0100

    [FLINK-13191][table-common] Add function annotations

---
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/annotation/DataTypeHint.java
@@ -0,0 +1,236 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.annotation;
+
+import org.apache.flink.annotation.PublicEvolving;
+import org.apache.flink.api.common.typeutils.TypeSerializer;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.types.DataType;
+import org.apache.flink.table.types.inference.InputTypeValidator;
+import org.apache.flink.table.types.inference.TypeInference;
+import org.apache.flink.table.types.logical.LogicalType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A hint that influences the reflection-based extraction of a {@link DataType}.
+ *
+ * <p>Data type hints can parameterize or replace the default extraction logic of individual function parameters
+ * and return types, structured classes, or fields of structured classes. An implementer can choose to
+ * what extent the default extraction logic should be modified.
+ *
+ * The following examples show how to explicitly specify data types, how to parameterize the extraction
+ * logic, or how to accept any data type as an input data type:
+ *
+ * <p>{@code @DataTypeHint("INT")} defines an INT data type with a reflectively extracted conversion class.
+ *
+ * <p>{@code @DataTypeHint(value = "TIMESTAMP(3)", bridgedTo = java.sql.Timestamp.class)} defines a TIMESTAMP
+ * data type of millisecond precision with an explicit conversion class.
+ *
+ * <p>{@code @DataTypeHint(value = "RAW", rawSerializer = MyCustomSerializer.class)} defines a RAW data type
+ * with a custom serializer class.
+ *
+ * <p>{@code @DataTypeHint(version = V1, allowRawGlobally = TRUE)} parameterizes the extraction by requesting
+ * a extraction logic version of 1 and allowing the RAW data type in this structured type (and possibly
+ * nested fields).
+ *
+ * <p>{@code @DataTypeHint(allowAnyInput = TRUE)} defines that the input validation should accept any
+ * data type.
+ *
+ * <p>Note: All hint parameters are optional. Hint parameters defined on top of a structured type are
+ * inherited by all (deeply) nested fields unless annotated differently. For example, all occurrences of
+ * {@link java.math.BigDecimal} will be extracted as {@code DECIMAL(12, 2)} if the enclosing class is
+ * annotated with {@code @DataTypeHint(defaultDecimalPrecision = 12, defaultDecimalScale = 2)}. Individual
+ * field annotations allow to deviate from those default values.
+ *
+ * @see FunctionHint
+ */
+@PublicEvolving
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+public @interface DataTypeHint {
+
+	// Note to implementers:
+	// Because "null" is not supported as an annotation value. Every annotation parameter has
+	// some representation for unknown values in order to merge multi-level annotations.
+
+	// --------------------------------------------------------------------------------------------
+	// Explicit data type specification
+	// --------------------------------------------------------------------------------------------
+
+	/**
+	 * The string representation of a data type. See {@link DataTypes} for a list of supported data
+	 * types. For example, {@code INT} for an integer data type or {@code DECIMAL(12, 5)} for decimal
+	 * data type with precision 12 and scale 5.
+	 *
+	 * <p>Use an unparameterized {@code RAW} string for explicitly declaring an opaque data type. For
+	 * Flink's default RAW serializer, use {@code @DataTypeHint("RAW")}. For a custom RAW serializer,
+	 * use {@code @DataTypeHint(value = "RAW", rawSerializer = MyCustomSerializer.class)}.
+	 *
+	 * <p>By default, the empty string represents an undefined data type.
+	 *
+	 * @see LogicalType#asSerializableString()
+	 * @see DataTypes
+	 */
+	String value() default "";
+
+	/**
+	 * Adds a hint that data should be represented using the given class when entering or leaving
+	 * the table ecosystem.
+	 *
+	 * <p>A supported conversion class depends on the logical type and its nullability property.
+	 *
+	 * <p>Please see the implementation of {@link LogicalType#supportsInputConversion(Class)},
+	 * {@link LogicalType#supportsOutputConversion(Class)}, or the documentation for more information
+	 * about supported conversions.
+	 *
+	 * <p>By default, the conversion class is reflectively extracted.
+	 * 
+	 * @see DataType#bridgedTo(Class)
+	 */
+	Class<?> bridgedTo() default void.class;
+
+	/**
+	 * Adds a hint that defines a custom serializer that should be used for serializing and deserializing
+	 * the opaque RAW type. It is only used if {@link #value()} is defined as an unparameterized {@code RAW}
+	 * string.
+	 *
+	 * <p>By default, Flink's default RAW serializer is used.
+	 *
+	 * @see DataTypes#ANY(Class, TypeSerializer)
+	 */
+	Class<?> rawSerializer() default void.class;
+
+	// --------------------------------------------------------------------------------------------
+	// Any input data type specification
+	// --------------------------------------------------------------------------------------------
+
+	/**
+	 * Adds a hint for enabling input wildcards during the extraction of a {@link TypeInference} in
+	 * functions. If set to {@code TRUE}, any data type can be passed if the annotated element is used
+	 * as an input parameter of a function. The behavior is similar to an always passing {@link InputTypeValidator}.
+	 *
+	 * <p>Note: The class of the annotated element must be {@link Object} as this is the super class
+	 * of all possibly passed data types. This annotation is only allowed as a top-level hint and is
+	 * ignored within nested structured types.
+	 */
+	HintFlag takeAnyInput() default HintFlag.UNKNOWN;
+
+	// --------------------------------------------------------------------------------------------
+	// Parameterization of the reflection-based extraction
+	// --------------------------------------------------------------------------------------------
+
+	/**
+	 * Version that describes the expected behavior of the reflection-based data type extraction.
+	 *
+	 * <p>It is meant for future backward compatibility. Whenever the extraction logic is changed,
+	 * old function and structured type classes should still return the same data type as before when
+	 * versioned accordingly.
+	 *
+	 * <p>By default, the version is always the most recent one.
+	 */
+	ExtractionVersion version() default ExtractionVersion.UNKNOWN;
+
+	/**
+	 * Defines that a RAW data type should be used for all classes that cannot be mapped to any SQL-like
+	 * data type or cause an error.
+	 *
+	 * <p>By default, this parameter is set to {@code false} which means that an exception is thrown
+	 * for unmapped types. This is helpful to identify and fix faulty implementations. It is generally
+	 * recommended to use SQL-like types instead of enabling RAW opaque types.
+	 *
+	 * <p>This parameter has higher precedence than {@link #allowRawPattern()}.
+	 *
+	 * @see DataTypes#ANY(Class, TypeSerializer)
+	 */
+	HintFlag allowRawGlobally() default HintFlag.UNKNOWN;
+
+	/**
+	 * Defines that a RAW data type should be used for all classes that cannot be mapped to any SQL-like
+	 * data type or cause an error iff their class name starts with or is equal to one of the given patterns.
+	 *
+	 * <p>For example, if some Joda time classes cannot be mapped to any SQL-like data type, one can
+	 * define the class pattern {@code "org.joda.time"}. Some classes might be handled as structured types
+	 * on a best effort basis but others will be RAW data types if necessary.
+	 *
+	 * <p>By default, the pattern list is empty which means that an exception is thrown for unmapped
+	 * types. This is helpful to identify and fix faulty implementations. It is generally recommended
+	 * to use SQL-like types instead of enabling RAW opaque types.
+	 *
+	 * <p>This parameter has lower precedence than {@link #allowRawGlobally()}.
+	 *
+	 * @see DataTypes#ANY(Class, TypeSerializer)
+	 */
+	String[] allowRawPattern() default {""};
+
+	/**
+	 * Defines that a RAW data type should be used for all classes iff their class name starts with or
+	 * is equal to one of the given patterns.
+	 *
+	 * <p>For example, one can define the class pattern {@code "org.joda.time", "java.math.BigDecimal"} which
+	 * means that all Joda time classes and Java's {@link java.math.BigDecimal} will be handled as RAW data
+	 * types regardless if they could be mapped to a more SQL-like data type.
+	 *
+	 * <p>By default, the pattern list is empty which means that an exception is thrown for unmapped
+	 * types. This is helpful to identify and fix faulty implementations. It is generally recommended
+	 * to use SQL-like types instead of enabling RAW opaque types.
+	 *
+	 * <p>This parameter has the highest precedence of all hint parameters.
+	 *
+	 * @see DataTypes#ANY(Class, TypeSerializer)
+	 */
+	String[] forceRawPattern() default {""};
+
+	/**
+	 * Defines a default precision for all decimal data types that are extracted.
+	 *
+	 * <p>By default, decimals are not extracted from classes such as {@link java.math.BigDecimal} because
+	 * they don't define a fixed precision and scale which is required in the SQL type system.
+	 */
+	int defaultDecimalPrecision() default -1;
+
+	/**
+	 * Defines a default scale for all decimal data types that are extracted.
+	 *
+	 * <p>By default, decimals are not extracted from classes such as {@link java.math.BigDecimal} because
+	 * they don't define a fixed precision and scale which is required in the SQL type system.
+	 */
+	int defaultDecimalScale() default -1;
+
+	/**
+	 * Defines a default year precision for all year-month intervals that are extracted. If set to {@code 0},
+	 * an {@code INTERVAL MONTH} data type is extracted.
+	 *
+	 * <p>By default, {@code INTERVAL YEAR(4) TO MONTH} data types are extracted from classes such as
+	 * {@link java.time.Period}.
+	 */
+	int defaultYearPrecision() default -1;
+
+	/**
+	 * Defines a default fractional second precision for all day-time intervals and timestamps that
+	 * are extracted.
+	 *
+	 * <p>By default, those data types are extracted with nano second precision.
+	 */
+	int defaultSecondPrecision() default -1;
+
+}
---
+++ flink-table/flink-table-common/src/main/java/org/apache/flink/table/annotation/FunctionHint.java
@@ -0,0 +1,155 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.table.annotation;
+
+import org.apache.flink.annotation.PublicEvolving;
+import org.apache.flink.table.functions.UserDefinedFunction;
+import org.apache.flink.table.types.inference.TypeInference;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A hint that influences the reflection-based extraction of a {@link TypeInference} for a
+ * {@link UserDefinedFunction}.
+ *
+ * <p>A function hint can parameterize or replace the default extraction logic for input types, accumulator
+ * types, and output types. An implementer can choose to what extent the default extraction logic
+ * should be modified.
+ *
+ * <p>One or more annotations can be declared on top of a {@link UserDefinedFunction} class or individually
+ * for each {@code eval()/accumulate()} method for overloading function signatures. All hint parameters
+ * are optional. If a parameter is not defined, the default reflection-based extraction is used. Hint
+ * parameters defined on top of a {@link UserDefinedFunction} class are inherited by all {@code eval()/accumulate()}
+ * methods unless annotated differently.
+ *
+ * <p>The following examples show how to explicitly specify function signatures in whole or in part
+ * and let the default extraction do the rest:
+ *
+ * <pre>
+ * {@code
+ *   // accepts (INT, STRING) and returns BOOLEAN
+ *   @FunctionHint(
+ *     input = [@DataTypeHint("INT"), @DataTypeHint("STRING")],
+ *     output = @DataTypeHint("BOOLEAN")
+ *   )
+ *   class ScalarFunction { ... }
+ *
+ *   // accepts (INT, STRING) or (BOOLEAN) and returns BOOLEAN
+ *   @FunctionHint(
+ *     input = [@DataTypeHint("INT"), @DataTypeHint("STRING")],
+ *     output = @DataTypeHint("BOOLEAN")
+ *   )
+ *   @FunctionHint(
+ *     input = [@DataTypeHint("BOOLEAN")],
+ *     output = @DataTypeHint("BOOLEAN")
+ *   )
+ *   class ScalarFunction { ... }
+ *
+ *   // accepts (INT, STRING) or (BOOLEAN) and returns BOOLEAN
+ *   @FunctionHint(
+ *     output = @DataTypeHint("BOOLEAN")
+ *   )
+ *   class ScalarFunction {
+ *     @FunctionHint(
+ *       input = [@DataTypeHint("INT"), @DataTypeHint("STRING")]
+ *     )
+ *     @FunctionHint(
+ *       input = [@DataTypeHint("BOOLEAN")]
+ *     )
+ *     Object eval(Object... o) { ... }
+ *   }
+ *
+ *   // accepts (INT) or (BOOLEAN) and returns ROW<f0 BOOLEAN, f1 INT>
+ *   @FunctionHint(
+ *     output = @DataTypeHint("ROW<f0 BOOLEAN, f1 INT>")
+ *   )
+ *   class ScalarFunction {
+ *     Row eval(int i) { ... }
+ *     Row eval(boolean b) { ... }
+ *   }
+ *
+ *   // accepts (ROW<f BOOLEAN>...) or (BOOLEAN...) and returns INT
+ *   class ScalarFunction {
+ *     @FunctionHint(
+ *       input = [@DataTypeHint("ROW<f BOOLEAN>")],
+ *       isVarArgs = TRUE
+ *     )
+ *     int eval(Row... r) { ... }
+ *
+ *     int eval(boolean... b) { ... }
+ *   }
+ * }
+ * </pre>
+ *
+ * @see DataTypeHint
+ */
+@PublicEvolving
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Repeatable(FunctionHints.class)
+public @interface FunctionHint {
+
+	// Note to implementers:
+	// Because "null" is not supported as an annotation value. Every annotation parameter has
+	// some representation for unknown values in order to merge multi-level annotations.
+
+	/**
+	 * Explicitly lists the argument types that a function takes as input.
+	 *
+	 * <p>By default, explicit input types are undefined and the reflection-based extraction is
+	 * used.
+	 */
+	DataTypeHint[] input() default @DataTypeHint();
+
+	/**
+	 * Defines that the last argument type defined in {@link #input()} should be treated as a
+	 * variable-length argument.
+	 *
+	 * <p>By default, if {@link #input()} is defined, the last argument type is not a var-arg. Otherwise
+	 * the reflection-based extraction decides about var-arg parameters.
+	 */
+	HintFlag isVarArgs() default HintFlag.UNKNOWN;
+
+	/**
+	 * Explicitly lists the argument names that a function takes as input.
+	 *
+	 * <p>By default, explicit input names are undefined and the reflection-based extraction is used.
+	 */
+	String[] argumentNames() default {""};
+
+	/**
+	 * Explicitly defines the intermediate result type that a function uses as accumulator.
+	 *
+	 * <p>By default, an explicit accumulator type is undefined and the reflection-based extraction
+	 * is used.
+	 */
+	DataTypeHint accumulator() default @DataTypeHint();
+
+	/**
+	 * Explicitly defines the result type that a function uses as output.
+	 *
+	 * <p>By default, an explicit output type is undefined and the reflection-based extraction
+	 * is used.
+	 */
+	DataTypeHint output() default @DataTypeHint();
+}
