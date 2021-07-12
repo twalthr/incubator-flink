48763f861bd547915049408deb6f538c3cccf81f
Timo Walther <twalthr@apache.org>
2021-07-12 16:23:33 +0200

    backup

--- flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/utils/ExecutorUtils.java
+++ flink-table/flink-table-planner/src/main/java/org/apache/flink/table/planner/utils/ExecutorUtils.java
@@ -69,7 +69,7 @@ public class ExecutorUtils {
                 .getStreamNodes()
                 .forEach(sn -> sn.setResources(ResourceSpec.UNKNOWN, ResourceSpec.UNKNOWN));
         streamGraph.setChaining(true);
-        streamGraph.setAllVerticesInSameSlotSharingGroupByDefault(false);
+        streamGraph.setAllVerticesInSameSlotSharingGroupByDefault(true);
         // Configure job type for properly selecting a supported scheduler for batch jobs.
         // LAZY_FROM_SOURCES_WITH_BATCH_SLOT_REQUEST is only supported by the Batch scheduler (=Ng
         // scheduler)
